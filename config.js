693fc23a7291050f4000552904e29e982b12f9b5
Connor Bond <connorbond@driveworks.co.uk>
2022-03-16 15:37:07 +0000

    Add help text to config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+// Update these values to match those of your Server URL, DriveWorks Group Alias
+// and Project Name that you wish to be rendered.
+
 const config = {
     serverUrl: "",
     groupAlias: "",
